716203a99b4c4d20b8f583bc81ea9738144ad546
Ivan <ivan.zhmurko.04@gmail.com>
2024-12-03 19:36:00 +0300

    Add JavaDoc

---
+++ src/main/java/org/example/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Tester"},{"p":"org.example","l":"TesterTest"}];updateSearchResults();
